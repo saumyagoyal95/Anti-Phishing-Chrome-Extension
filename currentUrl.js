095bae1aa7b849217b479b2d87d7453391090f0d
killerCult <soccer.skt@gmail.com>
2016-03-25 17:29:15 +0530

    Update currentUrl.js

--- currentUrl.js
+++ currentUrl.js
@@ -1,8 +1,22 @@
 //grab the current url
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
 
-   chrome.tabs.getSelected(null, function(tab) {
-       var tabId = tab.id;
-       tabUrl = tab.url;
+    chrome.tabs.getSelected(null, function(tab) {
+    
+		var tabId = tab.id;
+		tabUrl = tab.url;
 
-       document.write(tabUrl);
-   });
+		var xhr = new XMLHttpRequest();
+		//xhr.onreadystatechange = function() {
+		//if (xhr.readyState==4 && xhr.status==200) {
+		//  document.getElementById("disp").innerHTML=xhr.responseText;
+		//	}
+		//} 
+		xhr.open("GET", chrome.extension.getURL('http://localhost/insertdb.php'), true);
+		xhr.send();
+		   
+	});
+  }, false);
+}, false);
